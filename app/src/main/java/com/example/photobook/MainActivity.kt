5b9086541cd1aa25a86afd8989d16f5ab8d4c251
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-05-21 10:00:25 +0300

    add files created

--- app/src/main/java/com/example/photobook/MainActivity.kt
+++ app/src/main/java/com/example/photobook/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnNext1: Button
+    lateinit var btnNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
